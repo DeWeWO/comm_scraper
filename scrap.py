ade176774a1fd29451c040a249b8799c27b3c368
dewew <dewel000per@gmail.com>
2025-04-07 21:19:13 +0500

    scraper

---
+++ scrap.py
@@ -0,0 +1,22 @@
+from google_play_scraper import Sort, reviews
+import pandas as pd
+
+# Ilova ID'si
+app_id = "org.telegram.messenger" 
+
+result, _ = reviews(
+    app_id,
+    lang='uz',      # Izohlar qaysi tilda bo‘lishi
+    country='us',
+    sort=Sort.NEWEST,
+    count=100       # Nechta izoh yuklansin
+)
+
+# Faqat yulduzlar va izoh matnlarini ajratib olish
+data = [{'score': r['score'], 'content': r['content']} for r in result]
+
+df = pd.DataFrame(data)
+
+df.to_csv("tg_comm.csv", index=False, encoding='utf-8-sig')
+
+print(".csv faylga comment lar saqlandi.")
